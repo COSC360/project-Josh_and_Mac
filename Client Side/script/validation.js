77770c219f4dec927781bde238267d60d7ed082e
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-02-28 21:36:34 -0800

    feat: new validation

--- Client Side/script/validation.js
+++ Client Side/script/validation.js
@@ -26,5 +26,54 @@ window.addEventListener("load", function(){
     }
   }
   );
+});
 
-  });
+window.addEventListener("load", function(){
+  document.getElementById('signupForm').addEventListener('submit', function(event) {
+  var username = document.getElementsByName('username')[0].value;
+  var password = document.getElementsByName('password')[0].value;
+  var name = document.getElementsByName('name')[0].value;
+  var email = document.getElementsByName('email')[0].value;
+  if (username ==='' || password === '' || name==='' || email==='') {
+     event.preventDefault();
+     alert('Please fill in the required highlighted fields.');
+    if (username === ''){
+      document.getElementsByName("username")[1].classList.add("highlight");
+    }
+    if (password === ""){
+      document.getElementsByName("password")[1].classList.add("highlight");
+    }
+    if (name === ""){
+      document.getElementsByName("name")[0].classList.add("highlight");
+    }
+    if (email === ""){
+      document.getElementsByName("email")[0].classList.add("highlight");
+    }
+  }
+});
+document.getElementsByName("username")[1].addEventListener("input", function(e){
+  if (this.value !==""){
+      this.classList.remove("highlight");
+  }
+}
+);
+document.getElementsByName("name")[0].addEventListener("input", function(e){
+  if (this.value !==""){
+      this.classList.remove("highlight");
+  }
+}
+);
+document.getElementsByName("email")[0].addEventListener("input", function(e){
+  if (this.value !==""){
+      this.classList.remove("highlight");
+  }
+}
+);
+
+document.getElementsByName("password")[1].addEventListener("input", function(e){
+  if (this.value !==""){
+      this.classList.remove("highlight");
+  }
+}
+);
+});
