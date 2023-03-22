7d4d831c71fb6c2609a37bdfcf0382c446c8dab5
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-03-22 09:13:34 -0700

    feat: removing C comment data on delete

--- Client Side/HTML/deleteAccount.php
+++ Client Side/HTML/deleteAccount.php
@@ -3,11 +3,14 @@ include "connectDB.php";
 session_start();
 if (isset($_SESSION["id"]) && isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"])===true){
 $stmt = $con->prepare('DELETE FROM account WHERE id = ?');
+$stmt2 = $con->prepare('DELETE FROM comments WHERE name = ?');
 // In this case we can use the search to get the account info.
 $cid = $_SESSION['id'];
+$username = $_SESSION['name'];
 $stmt->bind_param('i', $cid);
+$stmt2->bind_param('s', $username);
 //$stmt->execute();
-if($stmt->execute()){
+if($stmt->execute() && $stmt2->execute()){
     echo "<h3>Customer account deleted successfully."
         . " Please browse your localhost php my admin"
         . " to view the updated data</h3>";
--- Client Side/HTML/deleteCustomerAdmin.php
+++ Client Side/HTML/deleteCustomerAdmin.php
@@ -3,11 +3,14 @@
 // if (isset($_SESSION["id"]) && isset($_SESSION["is_admin"]) && $_SESSION["is_admin"] === true)
 include "connectDB.php";
 $stmt = $con->prepare('DELETE FROM account WHERE id = ?');
+$stmt2 = $con->prepare('DELETE FROM comments WHERE name = ?');
 // In this case we can use the search to get the account info.
 $cid = $_GET['id'];
+$username = $_GET['username'];
 $stmt->bind_param('i', $cid);
+$stmt2->bind_param('s', $username);
 //$stmt->execute();
-if($stmt->execute()){
+if($stmt->execute() && $stmt2->execute()){
     echo "<h3>Customer account deleted successfully."
         . " Please browse your localhost php my admin"
         . " to view the updated data</h3>";
