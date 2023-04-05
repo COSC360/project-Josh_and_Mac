96258da6b37736743a999668db9bce91564a6c79
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-04-05 11:03:25 -0700

    feat: test for deleteAccount

--- Client Side/PHP/deleteAccount.php
+++ Client Side/PHP/deleteAccount.php
@@ -1,26 +1,58 @@
 <?php
-include "connectDB.php";
-session_start();
-if (isset($_SESSION["id"]) && isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"])===true){
-    $stmt = $con->prepare('DELETE FROM account WHERE id = ?');
-    // In this case we can use the search to get the account info.
-    $cid = $_SESSION['id'];
-    $username = $_SESSION['username'];
-    $stmt->bind_param('i', $cid);
-    //$stmt->execute();
-    if($stmt->execute()){
-            session_destroy();
-            $_SESSION["newuser_msg"] = "Account: $username has been deleted";
-            header('Location: login.php');
+// include "connectDB.php";
+// session_start();
+// if (isset($_SESSION["id"]) && isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"])===true){
+//     $stmt = $con->prepare('DELETE FROM account WHERE id = ?');
+//     // In this case we can use the search to get the account info.
+//     $cid = $_SESSION['id'];
+//     $username = $_SESSION['username'];
+//     $stmt->bind_param('i', $cid);
+//     //$stmt->execute();
+//     if($stmt->execute()){
+//             session_destroy();
+//             $_SESSION["newuser_msg"] = "Account: $username has been deleted";
+//             header('Location: login.php');
 
-    } else{
-        echo "ERROR "
-            . mysqli_error($con);
-    }
-}
-else echo "Not logged in";
+//     } else{
+//         echo "ERROR "
+//             . mysqli_error($con);
+//     }
+// }
+// else echo "Not logged in";
 
  
-$stmt->close();
-mysqli_close($con);
+// $stmt->close();
+// mysqli_close($con);
+?>
+
+<?php
+include "connectDB.php";
+session_start();
+
+function deleteAccount($con)
+{
+    if (isset($_SESSION["id"]) && isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"])===true){
+        $stmt = $con->prepare('DELETE FROM account WHERE id = ?');
+        // In this case we can use the search to get the account info.
+        $cid = $_SESSION['id'];
+        $username = $_SESSION['username'];
+        $stmt->bind_param('i', $cid);
+        //$stmt->execute();
+        if($stmt->execute()){
+                session_destroy();
+                $_SESSION["newuser_msg"] = "Account: $username has been deleted";
+                header('Location: login.php');
+        } else{
+            echo "ERROR " . mysqli_error($con);
+        }
+    }
+    else {
+        echo "Not logged in";
+    }
+
+    $stmt->close();
+    mysqli_close($con);
+}
+
+deleteAccount($con);
 ?>
--- Client Side/PHP/testCases.php
+++ Client Side/PHP/testCases.php
@@ -22,4 +22,21 @@ class Tests extends PHPUnit\Framework\TestCase
         $con = new mysqli($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
         $this->assertTrue($con->connect_errno === 0);
     }
+
+    public function testDeleteAccount()
+    {
+        // Arrange
+        $con = new mysqli('localhost', 'username', 'password', 'database');
+        $_SESSION["id"] = 1;
+        $_SESSION["loggedin"] = true;
+        $_SESSION["username"] = "testuser";
+
+        // Act
+        deleteAccount($con);
+
+        // Assert
+        $this->assertEmpty($_SESSION);
+        $this->assertContains("Account: testuser has been deleted", $_SESSION["newuser_msg"]);
+        $this->assertEquals('login.php', $this->getResponseHeader('Location'));
+    }
 }
